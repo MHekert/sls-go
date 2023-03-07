0df6cd9c6f93d3b680e7c9b7818f40b1ab882b08
Mateusz Hekert <mateusz.hekert@gmail.com>
2023-03-07 20:36:55 +0100

    feat: handle individual s3 imports in goroutines

--- src/items/driving/import-items.s3-lambda.go
+++ src/items/driving/import-items.s3-lambda.go
@@ -3,17 +3,24 @@ package items
 import (
 	"context"
 	service "sls-go/src/items/core/service"
+	"sync"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 func HandlerFactory(workersCount int, useCase *service.ItemsImporterUseCase) func(context.Context, events.S3Event) {
 	return func(ctx context.Context, s3Event events.S3Event) {
-		for recordIndex := range s3Event.Records {
-			s3data := s3Event.Records[recordIndex].S3
+		var wg sync.WaitGroup
+		wg.Add(len(s3Event.Records))
 
-			useCase.Do(workersCount, s3data.Object.Key)
+		for recordIndex := range s3Event.Records {
+			key := s3Event.Records[recordIndex].S3.Object.Key
+			go func(wg *sync.WaitGroup) {
+				useCase.Do(workersCount, key)
+				wg.Done()
+			}(&wg)
 		}
 
+		wg.Wait()
 	}
 }
